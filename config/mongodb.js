750f6d44c9ea1d8c2418eb121864685d9063b9ab
Muhammad Bin Saeed <45627082+muhammad-bin-saeed@users.noreply.github.com>
2022-05-23 23:08:07 +0500

    initial commit

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/file-sharing";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const dbConnect = async () => {
   try {
